1c57ea9c3386b75ed649cf3f2d2266d636ea2fa8
RickardPegado <rickard.pegado.mte23lin@edu.tucsweden.se>
2023-12-29 17:09:39 +0100

    hgh

--- Edge/test_checkout_swag_labs.py
+++ Edge/test_checkout_swag_labs.py
@@ -12,7 +12,9 @@ class TestUrl:
 
     @pytest.fixture
     def launch_driver(self):
-        self.driver = webdriver.Edge()
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Edge(options=options)
         self.driver.maximize_window()
         self.driver.implicitly_wait(8)
         yield
